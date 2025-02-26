7479b30e8d7d11b999fcb1bb364e099b25951f26
Heavy-Bubbles <chelseatanxuanlin@gmail.com>
2025-02-27 00:43:48 +0800

    frontend in progress

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,47 @@
+import { Container, Flex, Text, HStack, Button, Icon } from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
+import { FiPlusSquare } from "react-icons/fi"
+import { ColorModeButton } from './ui/color-mode'
+
+const Navbar = () => {
+    console.log("IS RENDERED")
+  return (
+        <Container maxW={"1140px"} px={4}>
+           <Flex
+                h={16}
+                alignItems={"center"}
+                justifyContent={"space-between"}
+                flexDir={{
+                    base:"column",
+                    sm:"row"
+                }}
+            >
+                <Text
+                    fontSize={{base:22, sm: 28}}
+                    fontWeight={"bold"}
+                    textTransform={"uppercase"}
+                    textAlign={"center"}
+                    bgGradient={"to-r"}
+                    gradientFrom={"cyan.400"}
+                    gradientTo={"blue.500"}
+                    bgClip={"text"}
+                >
+                    <Link to={"/"}>Product Store 🛒</Link>
+                </Text>
+                <HStack spacing={2} alignItems={"center"}>
+                    <Link to={"/create"}>
+                        <Button >
+                            <Icon
+                                fontSize={20}>
+                                    <FiPlusSquare />
+                            </Icon>
+                        </Button>
+                    </Link>
+                    <ColorModeButton />
+                </HStack>
+           </Flex>
+        </Container>
+  )
+}
+
+export default Navbar
